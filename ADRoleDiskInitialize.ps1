62a73e40f39bb8160816994b485474d4f465c2fa
Arnav <arnav.gupta666@gmail.com>
2020-04-20 15:28:47 +0530

    Add files via upload

---
+++ ADRoleDiskInitialize.ps1
@@ -0,0 +1,12 @@
+﻿
+#Install AD role
+Install-WindowsFeature AD-Domain-Services
+
+#Initialize New Disk
+Initialize-Disk 2 –PartitionStyle MBR
+New-Partition -DiskNumber 2 -AssignDriveLetter –UseMaximumSize
+$partitions = Get-Partition
+$driveletter = $partitions[$partitions.Length - 1].DriveLetter
+
+#Format New Disk
+Format-Volume –DriveLetter $driveletter -FileSystem NTFS 
